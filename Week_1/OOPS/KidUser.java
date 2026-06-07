54b0d611deb220e7936f0d2cd3b5a4e511332504
Naman saini <102904170+Namansaini2531@users.noreply.github.com>
2026-06-07 20:22:49 +0530

    Update KidUser.java

--- Week_1/OOPS/KidUser.java
+++ Week_1/OOPS/KidUser.java
@@ -5,8 +5,9 @@ public class KidUser implements LibraryUser {
     @Override
     public void registerAccount() {
         if (age < 12) {
-            System.out.println("You have been successfully registered with a Kids Account");
-        } else {
+            System.out.println("You have successfully registered under a Kids Account");
+        } 
+        else {
             System.out.println("Sorry, Age must be less than 12 to register as a kid");
         }
     }
@@ -15,8 +16,9 @@ public class KidUser implements LibraryUser {
     public void requestBook() {
         if (bookType.equals("Kids")) {
             System.out.println("Book Issued successfully, please return the book within 10 days");
-        } else {
-            System.out.println("you are allowed to take only kids books");
+        } 
+        else {
+            System.out.println("Oops, you are allowed to take only kids books");
         }
     }
 }
