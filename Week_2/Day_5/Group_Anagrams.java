b803a84a630429bd13fd10c5fc04055edb946ffc
Naman saini <102904170+Namansaini2531@users.noreply.github.com>
2026-06-12 20:50:52 +0530

    Implement grouping of anagrams in Group_Anagrams.java

---
+++ Week_2/Day_5/Group_Anagrams.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String s : strs) {
+            char[] x = s.toCharArray();
+            Arrays.sort(x);
+            String key = new String(x);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
