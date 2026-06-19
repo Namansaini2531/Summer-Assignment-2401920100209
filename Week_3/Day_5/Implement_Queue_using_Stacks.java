dad2d754279c5206c4be7e6b25183466bb2ba3fe
Naman saini <102904170+Namansaini2531@users.noreply.github.com>
2026-06-19 23:40:40 +0530

    Create Implement_Queue_using_Stacks.java

---
+++ Week_3/Day_5/Implement_Queue_using_Stacks.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    private Stack<Integer> s1; 
+    private Stack<Integer> s2; 
+
+    public MyQueue() 
+    {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) 
+    {
+        s1.push(x);
+    }
+
+    public int pop() 
+    {
+        pour();
+        return s2.pop();
+    }
+
+    public int peek() 
+    {
+        pour();
+        return s2.peek();
+    }
+
+    public boolean empty() 
+    {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+
+    private void pour() {
+        if(s2.isEmpty()) {
+            while(!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+    }
+}
