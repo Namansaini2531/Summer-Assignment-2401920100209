f989e6446dec1145bd55c529c35378f66b4c1e1f
Naman saini <102904170+Namansaini2531@users.noreply.github.com>
2026-06-28 21:30:17 +0530

    Create ToTestInt.java

---
+++ Week_4/OOPS/ToTestInt.java
@@ -0,0 +1,7 @@
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic a = new Arithmetic();
+        int num = 6;
+        System.out.println("Square = " + a.square(num));
+    }
+}
